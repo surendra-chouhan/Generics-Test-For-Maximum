5dafcdab3b59b4a849505301cb1b72fb8e683dfc
surendra-chouhan <chouhansurendra028@gmail.com>
2021-03-09 19:59:22 +0530

    Refactored the code to have One Generic Method For All and find Maximum

--- src/main/java/com/genericsproblem/GenericsProblemForMaximum.java
+++ src/main/java/com/genericsproblem/GenericsProblemForMaximum.java
@@ -1,30 +1,13 @@
 package com.genericsproblem;
 
 public class GenericsProblemForMaximum {
-    public Integer getIntMaximum(Integer num1, Integer num2, Integer num3 ) {
-        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-            return num1;
-        else if (num2.compareTo(num3) > 0 && num2.compareTo(num1) > 0)
-            return num2;
+    public <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
+        if (x.compareTo(y) > 0 && x.compareTo(z) > 0)
+            return x;
+        else if (y.compareTo(z) > 0 && y.compareTo(x) > 0)
+            return y;
         else
-            return num3;
+            return z;
     }
 
-    public Double getDoubleMaximum(Double num1, Double num2, Double num3 ) {
-        if (num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
-            return num1;
-        else if (num2.compareTo(num3) > 0 && num2.compareTo(num1) > 0)
-            return num2;
-        else
-            return num3;
-    }
-
-    public String getStringMaximum(String str1, String str2, String str3) {
-        if (str1.compareTo(str2) > 0 && str1.compareTo(str3) > 0)
-            return str1;
-        else if (str2.compareTo(str3) > 0 && str2.compareTo(str1) > 0)
-            return str2;
-        else
-            return str3;
-    }
 }
--- src/test/java/com/genericsproblem/GenericsProblemForMaximumTest.java
+++ src/test/java/com/genericsproblem/GenericsProblemForMaximumTest.java
@@ -14,57 +14,57 @@ public class GenericsProblemForMaximumTest {
 
     @Test
     public void given_MaxNumberat_FirstPosition_Returns_True_Test() {
-        int result = max.getIntMaximum(30, 20, 10);
+        int result = max.getMaximum(30, 20, 10);
         Assert.assertEquals(30, result);
     }
 
     @Test
     public void given_MaxNumberat_SecondPosition_Returns_True_Test() {
-        int result = max.getIntMaximum(20, 30, 10);
+        int result = max.getMaximum(20, 30, 10);
         Assert.assertEquals(30, result);
     }
 
     @Test
     public void given_MaxNumberat_ThirdPosition_Returns_True_Test() {
-        int result = max.getIntMaximum(10, 20, 30);
+        int result = max.getMaximum(10, 20, 30);
         Assert.assertEquals(30, result);
     }
 
 
     @Test
     public void given_MaxDoubleat_FirstPosition_Returns_True_Test() {
-        double result = max.getDoubleMaximum(10.55, 6.5, 8.5);
+        double result = max.getMaximum(10.55, 6.5, 8.5);
         Assert.assertEquals(10.55, result, 0.001);
     }
 
     @Test
     public void given_MaxDoubleat_SecondPosition_Returns_True_Test() {
-        double result = max.getDoubleMaximum(8.5, 10.55, 6.5);
+        double result = max.getMaximum(8.5, 10.55, 6.5);
         Assert.assertEquals(10.55, result, 0.001);
     }
 
     @Test
     public void given_MaxDoubleat_ThirdPosition_Returns_True_Test() {
-        double result = max.getDoubleMaximum(8.5, 6.5, 10.55);
+        double result = max.getMaximum(8.5, 6.5, 10.55);
         Assert.assertEquals(10.55, result, 0.001);
     }
 
 
     @Test
     public void given_MaxStringat_FirstPosition_Returns_True_Test() {
-        String result = max.getStringMaximum("Peach", "Banana", "Apple");
+        String result = max.getMaximum("Peach", "Banana", "Apple");
         Assert.assertEquals("Peach", result);
     }
 
     @Test
     public void given_MaxStringat_SecondPosition_Returns_True_Test() {
-        String result = max.getStringMaximum("Apple","Peach","Banana");
+        String result = max.getMaximum("Apple","Peach","Banana");
         Assert.assertEquals("Peach", result);
     }
 
     @Test
     public void given_MaxStringat_ThirdPosition_Returns_True_Test() {
-        String result = max.getStringMaximum("Apple", "Peach", "banana");
+        String result = max.getMaximum("Apple", "Peach", "banana");
         Assert.assertEquals("banana", result);
     }
 }
