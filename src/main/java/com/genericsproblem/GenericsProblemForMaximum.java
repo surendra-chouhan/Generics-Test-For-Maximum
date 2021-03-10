4f41c36cfde870ecf9b6df8b0ac52b1301dd406d
surendra-chouhan <chouhansurendra028@gmail.com>
2021-03-10 17:02:34 +0530

    Refactored the code to take more than 3 parameters and return maximum using Options and Sort

--- src/main/java/com/genericsproblem/GenericsProblemForMaximum.java
+++ src/main/java/com/genericsproblem/GenericsProblemForMaximum.java
@@ -1,25 +1,18 @@
 package com.genericsproblem;
 
+import java.util.Arrays;
+
 public class GenericsProblemForMaximum<T extends Comparable<T>> {
 
-    T x, y, z;
+    T[] values;
 
-    public GenericsProblemForMaximum() {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    @SafeVarargs
+    public GenericsProblemForMaximum(T... values) {
+        this.values = values;
     }
 
-    public T getMaximum() {
-        return  GenericsProblemForMaximum.getMaximum(x,y,z);
-    }
-
-    public static <T extends Comparable<T>> T getMaximum(T x, T y, T z) {
-        if (x.compareTo(y) > 0 && x.compareTo(z) > 0)
-            return x;
-        else if (y.compareTo(z) > 0 && y.compareTo(x) > 0)
-            return y;
-        else
-            return z;
+    public <T extends Comparable<T>> T getMaximum(){
+        Arrays.sort(values);
+        return (T) values[values.length - 1];
     }
 }
--- src/test/java/com/genericsproblem/GenericsProblemForMaximumTest.java
+++ src/test/java/com/genericsproblem/GenericsProblemForMaximumTest.java
@@ -1,70 +1,27 @@
 package com.genericsproblem;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class GenericsProblemForMaximumTest {
-    GenericsProblemForMaximum max;
-
-    @Before
-    public void set() {
-        max = new GenericsProblemForMaximum();
+    @Test
+    public void given_MultipleIntegers_as_Input_Returns_Maximum() {
+        GenericsProblemForMaximum<Integer> max = new GenericsProblemForMaximum<>(50, 20, 10, 40, 30);
+        int result = max.getMaximum();
+        Assert.assertEquals(50, result);
     }
 
     @Test
-    public void given_MaxNumberat_FirstPosition_Returns_True_Test() {
-        int result = (int) max.getMaximum(30, 20, 10);
-        Assert.assertEquals(30, result);
+    public void given_MultipleDoubles_as_Input_Returns_Maximum() {
+        GenericsProblemForMaximum<Double> max = new GenericsProblemForMaximum<>(50.00, 20.6, 10.0, 40.55, 30.5);
+        double result = max.getMaximum();
+        Assert.assertEquals(50.00, result, 0.001);
     }
 
     @Test
-    public void given_MaxNumberat_SecondPosition_Returns_True_Test() {
-        int result = (int) max.getMaximum(20, 30, 10);
-        Assert.assertEquals(30, result);
-    }
-
-    @Test
-    public void given_MaxNumberat_ThirdPosition_Returns_True_Test() {
-        int result = (int) max.getMaximum(10, 20, 30);
-        Assert.assertEquals(30, result);
-    }
-
-
-    @Test
-    public void given_MaxDoubleat_FirstPosition_Returns_True_Test() {
-        double result = (double) max.getMaximum(10.55, 6.5, 8.5);
-        Assert.assertEquals(10.55, result, 0.001);
-    }
-
-    @Test
-    public void given_MaxDoubleat_SecondPosition_Returns_True_Test() {
-        double result = (double) max.getMaximum(8.5, 10.55, 6.5);
-        Assert.assertEquals(10.55, result, 0.001);
-    }
-
-    @Test
-    public void given_MaxDoubleat_ThirdPosition_Returns_True_Test() {
-        double result = (double) max.getMaximum(8.5, 6.5, 10.55);
-        Assert.assertEquals(10.55, result, 0.001);
-    }
-
-
-    @Test
-    public void given_MaxStringat_FirstPosition_Returns_True_Test() {
-        String result = (String) max.getMaximum("Peach", "Banana", "Apple");
-        Assert.assertEquals("Peach", result);
-    }
-
-    @Test
-    public void given_MaxStringat_SecondPosition_Returns_True_Test() {
-        String result = (String) max.getMaximum("Apple","Peach","Banana");
-        Assert.assertEquals("Peach", result);
-    }
-
-    @Test
-    public void given_MaxStringat_ThirdPosition_Returns_True_Test() {
-        String result = (String) max.getMaximum("Apple", "Peach", "banana");
-        Assert.assertEquals("banana", result);
+    public void given_MultipleStrings_as_Input_Returns_Maximum() {
+        GenericsProblemForMaximum<String> max = new GenericsProblemForMaximum<>("Apple", "Peach", "mango", "pineapple", "Banana");
+        String result = max.getMaximum();
+        Assert.assertEquals("pineapple", result);
     }
 }
